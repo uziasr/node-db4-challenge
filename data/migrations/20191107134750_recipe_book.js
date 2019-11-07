cad0b5dcd9c126533c129aebbe9eefecb381ba4a
Uzias Rivera <uziasrivera@gmail.com>
2019-11-07 14:17:30 -0800

    created the table

---
+++ data/migrations/20191107134750_recipe_book.js
@@ -0,0 +1,39 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('recipes', tbl=>{
+        tbl.increments();
+        tbl.string('name',255).notNullable();
+    })
+    .createTable('ingredients', tbl=>{
+        tbl.increments();
+        tbl.string('name').notNullable()
+    })
+    .createTable('recipes_ingredients', tbl=>{
+        tbl.increments();
+        tbl
+        .integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+        tbl
+        .integer('ingredient_id')
+        .unsigned()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+        tbl.integer('quantity').notNullable()
+        tbl.string('amount',255).notNullable()
+    })
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('recipes_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+};
